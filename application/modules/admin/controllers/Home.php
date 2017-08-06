02b122799c7040e7c3bbe83a8e81ab4fae52451e
aravindumasankar <aravindumasankar@github.com>
2017-08-07 02:08:34 +0530

    rmoved cat banners

--- application/modules/admin/controllers/Home.php
+++ application/modules/admin/controllers/Home.php
@@ -15,16 +15,13 @@ class Home extends Admin_Controller {
         public function categories()
 	{
 		$crud = $this->generate_crud('homepage_categories');
-                $crud->columns('id','category_name','status', 'image_url','bg_color', 'added_on');
+                $crud->columns('id','cat_identifier','category_name','status', 'image_url','bg_color', 'added_on');
                 $this->mPageTitle = 'Homepage Categories';
                 $crud->set_field_upload('image_url', 'assets/uploads/homepage/categories');
                 $crud->callback_column('bg_color',array($this,'renderColor'));  
                 $crud->callback_add_field('path',array($this,'addCategoryPath'));
                 $crud->callback_edit_field('path',array($this,'addCategoryPath'));
                 $crud->callback_column('image_url',array($this,'renderCategory'));
-                $crud->set_field_upload('banner_image_url', 'assets/uploads/homepage/category_banners/');
-                $crud->set_field_upload('prize_image_url', 'assets/uploads/homepage/category_prize/');
-                $crud->set_field_upload('sponsor_image_url', 'assets/uploads/homepage/category_sponsor/');
                   	$this->mPageTitle = 'Homepage Categories';
 				$this->render_crud();
 				}
@@ -33,15 +30,11 @@ class Home extends Admin_Controller {
                    $color_code = $row->bg_color;
              return "<div style='background-color:$color_code;width: 105px;height: 115px;'>$row->image_url</div>";
                 }
-                
-                
                 function addCategoryPath($value){
             $value = 'assets/uploads/homepage/categories/';
             $return = '<input type="text" name="path" value="'.$value.'" /> ';
             return $return;
-                
-      		  }
-                
+      		  }  
             public function renderCategory($value, $row){
              $site_url = $this->config->base_url();
              
